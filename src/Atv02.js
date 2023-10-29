74e7b16823cb28de68ee405eea41307b9fd4916d
Fabrício Tofano Bersan <75699710+ftofano@users.noreply.github.com>
2023-10-28 21:28:21 -0300

    Atividade 2

--- src/Atv02.js
+++ src/Atv02.js
@@ -1,14 +1,22 @@
 
 import {Link} from "react-router-dom"
+import Botao from "./botao";
+import Perfil from "./perfil";
+import './Atv02.css';
 
 
-export default function Atv01() {
+export default function Atv02() {
     return (
         
         <div>
-            <h1>Atividade 02 </h1>
-
             <Link to='/'>Voltar</Link>
+            <h1>Atividade 02 </h1>
+            <div className='container'>
+                <Perfil />
+                <Botao />
+
+            </div>            
+
         </div>
         
     );
