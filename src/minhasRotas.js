ded0cfaa79569a98f787eab2b91d7b1084733d49
Fabrício Tofano Bersan <75699710+ftofano@users.noreply.github.com>
2023-11-23 10:49:16 -0300

    Add files via upload

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -6,6 +6,12 @@ import Atv01 from "./Atv01";
 
 import Atv02 from "./Atv02";
 
+import Atv3 from "./Atv3";
+
+import Atv04 from "./Atv04";
+
+import Calculadora from "./Calculadora";    
+
 export default function MinhasRotas() {
     return (
         
@@ -14,6 +20,9 @@ export default function MinhasRotas() {
                 <Route path="/" element= {<Home />} />
                 <Route path="/Atv01" element= {<Atv01 />} />
                 <Route path="/Atv02" element= {<Atv02 />} />
+                <Route path="/Atv3" element= {<Atv3 />} />
+                <Route path="/Atv04" element= {<Atv04 />} />
+                <Route path= "/Calculadora" element={<Calculadora />}/>
             </Routes>
         </BrowserRouter >
 
