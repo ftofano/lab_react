543878f4bb931335d3a0ef29496887328eda653b
Fabrício Tofano Bersan <75699710+ftofano@users.noreply.github.com>
2023-11-23 14:42:15 -0300

    Add files via upload

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -2,89 +2,96 @@ import React, { useState } from 'react';
 import './Calculadora.css';
 import { Link } from 'react-router-dom';
 
-export default function Calculadora(){
+export default function Calculadora() {
 
-    const [num,setNun] = useState(0);
-    const [prevnum,setPrevNun] = useState(0);
-    const [inpoperador,setOperador] = useState();
+    const [num, setNun] = useState(0);
+    const [prevnum, setPrevNun] = useState(0);
+    const [inpoperador, setOperador] = useState();
 
-    function addNun(valor){
-
+    function addNun(valor) {
         var inputnum = valor.target.value;
-
-        if(num === 0){
+    
+        if (num === 0) {
             setNun(inputnum);
-        }
-        else{
+        } else {
             setNun(num + inputnum);
         }
-        
     }
-    function aC(num){
+
+    function aC(num) {
         setNun(0);
     }
-    function porcent(){
+    function porcent() {
         setNun(num / 100);
     }
-    function mudaSinal(){
-        if(num > 0){
+    function mudaSinal() {
+        if (num > 0) {
             setNun(-num);
         }
-        else
-        {
+        else {
             setNun(-(num));
         }
-        
+
     }
-    function operador(op){
+    function operador(op) {
         var inpOperador = op.target.value;
         setOperador(inpOperador);
         setPrevNun(num);
         setNun(0);
-
     }
-
-    function calcular(){
-
-        if(operador === '/'){
+    
+    function calcular() {
+        if (inpoperador === '/') {
             setNun(parseFloat(prevnum) / parseFloat(num));
-        }else if(operador === '*'){
+        } else if (inpoperador === '*') {
             setNun(parseFloat(prevnum) * parseFloat(num));
-
-        }else if(operador === '+'){
+        } else if (inpoperador === '+') {
             setNun(parseFloat(prevnum) + parseFloat(num));
-        }else if(operador === '-'){
+        } else if (inpoperador === '-') {
             setNun(parseFloat(prevnum) - parseFloat(num));
         }
     }
+    
 
-    return(
+    return (
         <container>
-        <div className='fundo'>
+            <div className='fundo'>
 
-                <h1 className='monitor'>{num}</h1>
-                <button onClick={aC}>AC</button>
-                <button onClick={mudaSinal}>+/-</button>
-                <button onClick={porcent}>%</button>
-                <button onClick={operador} value='/'>/</button>
-                <button onClick={addNun} value={7}>7</button>
-                <button onClick={addNun} value={8}>8</button>
-                <button onClick={addNun} value={9}>9</button>
-                <button onClick={operador}value='X'>X</button>
-                <button onClick={addNun} value={4}>4</button>
-                <button onClick={addNun} value={5}>5</button>
-                <button onClick={addNun} value={6}>6</button>
-                <button onClick={operador} value={'-'}>-</button>
-                <button onClick={addNun} value={1}>1</button>
-                <button onClick={addNun} value={2}>2</button>
-                <button onClick={addNun} value={3}>3</button>
-                <button  onClick={operador}value='+'>+</button>
-                <button onClick={addNun} value={0}>0</button>
-                <button onClick={addNun} value={'.'}>,</button>
-                <button  onClick={calcular} value='='>=</button>
+                <div className='moni'><h1 className='monitor'>{num}</h1></div>
+                <div className='teclado'>
+                <div className='fileira'>
+                <button className='cinza' onClick={aC}>AC</button>
+                <button className='cinza'  onClick={mudaSinal}>+/-</button>
+                <button className='cinza'  onClick={porcent}>%</button>
+                <button className='orange' onClick={operador} value='/'>/</button>
+                </div>
+                <div className='fileira'>
+                <button className='preto' onClick={addNun} value={7}>7</button>
+                <button className='preto'  onClick={addNun} value={8}>8</button>
+                <button  className='preto' onClick={addNun} value={9}>9</button>
+                <button className='orange'  onClick={operador} value='*'>X</button>
+                </div>
+                <div className='fileira'>
+                <button className='preto'  onClick={addNun} value={4}>4</button>
+                <button className='preto' onClick={addNun} value={5}>5</button>
+                <button  className='preto' onClick={addNun} value={6}>6</button>
+                <button className='orange'  onClick={operador} value={'-'}>-</button>
+                </div>
+                <div className='fileira'>
+                <button  className='preto' onClick={addNun} value={1}>1</button>
+                <button  className='preto' onClick={addNun} value={2}>2</button>
+                <button  className='preto' onClick={addNun} value={3}>3</button>
+                <button className='orange'  onClick={operador} value='+'>+</button>
+                </div>
+                <div className='fileira'>
+                <button  className='preto' onClick={addNun} value={0}>0</button>
+                <button  className='preto' onClick={addNun} value={'.'}>,</button>
+                <button className='orange' onClick={calcular} value='='>=</button>
+                </div>
+                </div>
                 <div className='voltar'><Link to='/'>Voltar</Link></div>
-        </div>
-        </container>        
+            </div>
+        </container>
     );
 
 
